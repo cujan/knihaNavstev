fdd4de98031c4f4423ddeec8ebd859f55c2da55b
Ján Holub <holubjan@gmail.com>
2016-03-31 09:36:48 +0200

    napojenie DB

--- app/presenters/UdajeRevirPresenter.php
+++ app/presenters/UdajeRevirPresenter.php
@@ -7,9 +7,19 @@ use App\Model;
 
 
 class UdajeRevirPresenter extends BasePresenter {
+    
+    /** @var Nette\Database\Context */
+    private $database;
+
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
+    
+    
     public function renderDefault()
 	{
-		$this->template->anyVariable = 'any value';
+		$this->template->udajeRevir = $this->database->table('revirUdaje');
 	}
     
 }
