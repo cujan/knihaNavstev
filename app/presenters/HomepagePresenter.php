4e1104fd3bf3860ef3bec7918ac26048dcefad61
Ján Holub <holubjan@gmail.com>
2016-05-05 22:09:44 +0200

    prehlad lovu

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -10,13 +10,23 @@ use App\Model;
 class HomepagePresenter extends BasePresenter
 {
 
-	public function renderDefault()
-	{
-		$this->template->anyVariable = 'any value';
+	  /** @var Nette\Database\Context */
+    private $database;
+
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+	
+	
+    }
+    public function renderDefault(){
 		
-		
-	}
         
+	        
+        $this->template->dnesneNavstevy = $this->database->table('navstevaReviru')->where('zdruzenieId = ? AND datumNavsteva = ?', $this->user->getIdentity()->zdruzenieId, date("Y:m:d"));
+       
+	
+    }
        
     
 
---
+++ app/presenters/PrehladLovuPresenter.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Presenters;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+use Nette;
+use App\Model;
+/**
+ * Description of PrehladLovuPresenter
+ *
+ * @author Holub Ján
+ */
+class PrehladLovuPresenter extends BasePresenter {
+    //put your code here
+}
